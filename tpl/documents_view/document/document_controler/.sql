bf14f5bf5269d631bc1b830d4b4f35ff0fc05235
Victor Melnyk <victor.melnyk.vm@gmail.com>
2014-04-06 16:38:32 +0000

    updated documents view

--- tpl/documents_view/document/document_controler/.sql
+++ tpl/documents_view/document/document_controler/.sql
@@ -5,7 +5,7 @@ SELECT
 FROM doc.doc_documents D
   INNER JOIN doc.doc_document_last_edits DLE
     ON  DLE.document_id = D.document_id
-    AND DLE.language_id = doc.fn_doc_language_id_get(:code, :l)
+    AND DLE.language_id = doc.fn_doc_language_id_get(:code, :language_code)
   INNER JOIN doc.doc_document_edits DE
     ON  DE.document_id = DLE.document_id
     AND DE.language_id = DLE.language_id
