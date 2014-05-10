8003d391310e8de1ee61badc38e4469fc8d06248
vm <vm@4fd6277f-220a-4e57-8695-e122d3c56dfd>
2014-05-10 10:25:17 +0000

    updated db

---
+++ trunk/_db/df.sql
@@ -0,0 +1,135 @@
+
+SET statement_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = off;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET escape_string_warning = off;
+
+CREATE SCHEMA df;
+
+SET search_path = df, pg_catalog;
+
+CREATE DOMAIN df_boolean AS boolean;
+
+CREATE DOMAIN df_id AS integer;
+
+CREATE DOMAIN df_integer AS integer;
+
+CREATE DOMAIN df_interval AS interval;
+
+CREATE DOMAIN df_money AS numeric(10,2);
+
+CREATE DOMAIN df_smallint AS smallint;
+
+CREATE DOMAIN df_string_large AS character varying(255);
+
+CREATE DOMAIN df_string_short AS character varying(50);
+
+CREATE DOMAIN df_text AS text;
+
+CREATE DOMAIN df_timestamp AS timestamp without time zone;
+
+CREATE DOMAIN df_tinyint AS smallint;
+
+CREATE DOMAIN df_tinyint_id AS smallint
+	CONSTRAINT chk_df_tinyint_id CHECK (((VALUE >= 0) OR (VALUE <= 255)));
+
+CREATE FUNCTION fn_df_next_pk_value_get(atable_schema name, atable_name name) RETURNS df_id
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+  lresult df.df_id;
+  lfield_name df.df_string_short;
+BEGIN
+  lfield_name = df.fn_df_pk_field_name_get(atable_schema, atable_name);
+
+  EXECUTE 'SELECT COALESCE(MAX(' || lfield_name || ') + 1, 1) FROM ' ||
+    atable_schema || '.' || atable_name
+  INTO lresult;
+
+  RETURN lresult;
+END;
+$$;
+
+CREATE FUNCTION fn_df_next_random_pk_value_get(atable_schema name, atable_name name) RETURNS df_id
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+  lis_exist df.df_boolean;
+  lresult df.df_id;
+  lfield_name df.df_string_short;
+BEGIN
+  lfield_name = df.fn_df_pk_field_name_get(atable_schema, atable_name);
+
+  LOOP
+    lresult = df.fn_df_random_id();
+
+    EXECUTE 'SELECT EXIST(SELECT * FROM ' ||
+      atable_schema || '.' || atable_name ||
+      ' WHERE ' || lfield_name || ' = ' || lresult || ')'
+    INTO lis_exist;
+
+    IF NOT lis_exist THEN
+      EXIT;
+    END IF;
+  END LOOP;
+
+  RETURN lresult;
+END;
+$$;
+
+CREATE FUNCTION fn_df_pk_field_name_get(atable_schema name, atable_name name) RETURNS df_string_short
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+  lresult df.df_string_short;
+BEGIN
+  SELECT KCU.column_name
+  INTO lresult
+  FROM information_schema.table_constraints TC
+    INNER JOIN information_schema.key_column_usage KCU
+      ON  KCU.table_catalog   = TC.table_catalog
+      AND KCU.table_schema    = TC.table_schema
+      AND KCU.table_name      = TC.table_name
+      AND KCU.constraint_name = TC.constraint_name
+  WHERE TC.table_catalog   = current_catalog
+    AND TC.table_schema    = atable_schema
+    AND TC.table_name      = atable_name
+    AND TC.constraint_type = 'PRIMARY KEY';
+
+  RETURN lresult;
+END;
+$$;
+
+CREATE FUNCTION fn_df_random_i() RETURNS df_integer
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+  RETURN TRUNC(RANDOM() * 2^32) - 2^31;
+END;
+$$;
+
+CREATE FUNCTION fn_df_random_id() RETURNS df_id
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+  RETURN 1 + TRUNC(RANDOM() * (2^31 - 1));
+END;
+$$;
+
+CREATE FUNCTION fn_df_timestamp_to_str(avalue df_timestamp) RETURNS df_string_short
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+  RETURN to_char(avalue, 'yyyy-MM-dd HH24:MI:SS');
+END;
+$$;
+
+CREATE FUNCTION fn_df_utc_timestamp() RETURNS df_timestamp
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+  RETURN clock_timestamp() AT TIME ZONE 'UTC';
+END;
+$$;
---
+++ trunk/_db/doc.sql
@@ -0,0 +1,227 @@
+
+SET statement_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = off;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET escape_string_warning = off;
+
+CREATE SCHEMA doc;
+
+SET search_path = doc, pg_catalog;
+
+CREATE DOMAIN doc_language_code AS character varying(2) NOT NULL;
+
+CREATE FUNCTION fn_doc_clear() RETURNS void
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+  DELETE FROM doc.doc_document_last_edits;
+  DELETE FROM doc.doc_document_edits;
+  DELETE FROM doc.doc_documents;
+  DELETE FROM doc.doc_languages;
+END;
+$$;
+
+CREATE FUNCTION fn_doc_language_id_get(adocument_code df.df_string_short, alanguage_code doc_language_code) RETURNS df.df_tinyint_id
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+  RETURN (
+    SELECT L.language_id
+    FROM doc.doc_documents D
+      INNER JOIN doc.doc_languages L
+        ON L.code = alanguage_code
+      INNER JOIN doc.doc_document_last_edits DLE
+        ON  DLE.document_id = D.document_id
+        AND DLE.language_id = L.language_id
+    WHERE D.code = adocument_code
+    UNION ALL
+    SELECT *
+    FROM (
+      SELECT L.language_id
+      FROM doc.doc_documents D
+        INNER JOIN doc.doc_languages L
+          ON TRUE
+        INNER JOIN doc.doc_document_last_edits DLE
+          ON  DLE.document_id = D.document_id
+          AND DLE.language_id = L.language_id
+      WHERE D.code = adocument_code
+      ORDER BY L.lno
+    ) D
+    LIMIT 1);
+END;
+$$;
+
+CREATE FUNCTION fn_doc_pack() RETURNS void
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+  lrecord record;
+BEGIN
+  DELETE FROM doc.doc_document_edits DE
+  WHERE NOT EXISTS(
+    SELECT *
+    FROM doc.doc_document_last_edits DLE
+    WHERE DLE.document_id  = DE.document_id
+      AND DLE.language_id  = DE.language_id
+      AND DLE.last_edit_id = DE.edit_id
+  );
+
+  UPDATE doc.doc_document_edits
+  SET edit_id = 1
+  WHERE edit_id <> 1;
+
+  FOR lrecord IN
+    SELECT
+      D.document_id,
+      ( SELECT COUNT(*)
+        FROM doc.doc_documents _D
+        WHERE _D.document_id <= D.document_id
+      ) count
+    FROM doc.doc_documents D
+    ORDER BY D.document_id
+  LOOP
+    UPDATE doc.doc_documents
+    SET document_id = lrecord.count
+    WHERE document_id = lrecord.document_id;
+  END LOOP;
+END;
+$$;
+
+CREATE FUNCTION t_doc_document_edits_ai() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+  UPDATE doc.doc_document_last_edits
+  SET last_edit_id  = NEW.edit_id
+  WHERE document_id = NEW.document_id
+    AND language_id = NEW.language_id;
+
+  IF NOT FOUND THEN
+    INSERT INTO doc.doc_document_last_edits (
+      document_id,
+      language_id,
+      last_edit_id)
+     VALUES (
+      NEW.document_id,
+      NEW.language_id,
+      NEW.edit_id);
+  END IF;
+
+  RETURN NEW;
+END;
+$$;
+
+CREATE FUNCTION t_doc_document_edits_bi() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+  SELECT COALESCE(MAX(DE.edit_id) + 1, 1)
+  INTO NEW.edit_id
+  FROM doc.doc_document_edits DE
+  WHERE DE.document_id = NEW.document_id
+    AND DE.language_id = NEW.language_id;
+
+  NEW.date_created = df.fn_df_utc_timestamp();
+
+  RETURN NEW;
+END;
+$$;
+
+CREATE FUNCTION t_doc_documents_bi() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+  NEW.document_id = df.fn_df_next_pk_value_get(TG_TABLE_SCHEMA, TG_TABLE_NAME);
+
+  IF NEW.is_published IS NULL THEN
+    NEW.is_published = FALSE;
+  END IF;
+
+  NEW.date_created = df.fn_df_utc_timestamp();
+  NEW.is_deleted = FALSE;
+
+  RETURN NEW;
+END;
+$$;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+CREATE TABLE doc_document_edits (
+    document_id df.df_id NOT NULL,
+    language_id df.df_tinyint_id NOT NULL,
+    edit_id df.df_id NOT NULL,
+    content df.df_text NOT NULL,
+    date_created df.df_timestamp NOT NULL,
+    page_meta df.df_string_large,
+    page_title df.df_string_large
+);
+
+CREATE TABLE doc_document_last_edits (
+    document_id df.df_id NOT NULL,
+    language_id df.df_tinyint_id NOT NULL,
+    last_edit_id df.df_id NOT NULL
+);
+
+CREATE TABLE doc_documents (
+    document_id df.df_id NOT NULL,
+    code df.df_string_short NOT NULL,
+    is_published df.df_boolean NOT NULL,
+    date_created df.df_timestamp NOT NULL,
+    is_deleted df.df_boolean NOT NULL
+);
+
+CREATE TABLE doc_languages (
+    language_id df.df_tinyint_id NOT NULL,
+    code doc_language_code NOT NULL,
+    title df.df_string_short NOT NULL,
+    lno df.df_tinyint NOT NULL
+);
+
+ALTER TABLE ONLY doc_document_edits
+    ADD CONSTRAINT pk_doc_document_edits PRIMARY KEY (document_id, language_id, edit_id);
+
+ALTER TABLE ONLY doc_documents
+    ADD CONSTRAINT pk_doc_documents PRIMARY KEY (document_id);
+
+ALTER TABLE ONLY doc_languages
+    ADD CONSTRAINT pk_doc_languages PRIMARY KEY (language_id);
+
+ALTER TABLE ONLY doc_document_last_edits
+    ADD CONSTRAINT pk_document_last_edit PRIMARY KEY (document_id, language_id);
+
+ALTER TABLE ONLY doc_document_last_edits
+    ADD CONSTRAINT uq_doc_document_last_edits UNIQUE (document_id, language_id, last_edit_id);
+
+ALTER TABLE ONLY doc_documents
+    ADD CONSTRAINT uq_doc_documents UNIQUE (code);
+
+ALTER TABLE ONLY doc_languages
+    ADD CONSTRAINT uq_doc_languages UNIQUE (code);
+
+CREATE TRIGGER t_doc_document_edits_ai
+    AFTER INSERT ON doc_document_edits
+    FOR EACH ROW
+    EXECUTE PROCEDURE t_doc_document_edits_ai();
+
+CREATE TRIGGER t_doc_document_edits_bi
+    BEFORE INSERT ON doc_document_edits
+    FOR EACH ROW
+    EXECUTE PROCEDURE t_doc_document_edits_bi();
+
+CREATE TRIGGER t_doc_documents_bi
+    BEFORE INSERT ON doc_documents
+    FOR EACH ROW
+    EXECUTE PROCEDURE t_doc_documents_bi();
+
+ALTER TABLE ONLY doc_document_edits
+    ADD CONSTRAINT fk_doc_document_edits__doc_documents FOREIGN KEY (document_id) REFERENCES doc_documents(document_id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE ONLY doc_document_edits
+    ADD CONSTRAINT fk_doc_document_edits__doc_languages FOREIGN KEY (language_id) REFERENCES doc_languages(language_id);
+
+ALTER TABLE ONLY doc_document_last_edits
+    ADD CONSTRAINT fk_doc_document_last_edit__doc_document_edits FOREIGN KEY (document_id, language_id, last_edit_id) REFERENCES doc_document_edits(document_id, language_id, edit_id) ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED;
